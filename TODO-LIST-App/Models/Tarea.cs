544ebe11ff7ddc8e92a449c6c7abab0e7aa32516
agussantillan01 <agusantillan01@hotmail.com>
2025-03-31 17:08:59 -0300

    AS - Agrego clase Tarea

---
+++ TODO-LIST-App/Models/Tarea.cs
@@ -0,0 +1,9 @@
+﻿namespace TODO_LIST_App.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public bool Completado { get; set; }
+    }
+}
---
+++ TODO-LIST-App/Pages/Tarea/Tareas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TODO_LIST_App.Pages.Tarea
+{
+    public class TareasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
